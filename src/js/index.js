91bb383cecfa5016ada9e4455f2351bff7af249a
euny0ung <hdtv0218@gmail.com>
2023-03-27 17:58:59 +0900

    refactor : fix names - 재사용을 위해 espresso가 들어간 변수 이름 모두 변경

--- src/js/index.js
+++ src/js/index.js
@@ -17,14 +17,14 @@
 // - [x] 총 메뉴 개수를 count하여 상단에 보여준다.
 
 // TODO localStorage Read & Write
-// - [] localStorage에 데이터를 저장한다. write
-// - [] 메뉴를 수정할 때 저장
-// - [] 메뉴를 추가할 때 저장
-// - [] 메뉴를 삭제할 때 저장
-// - [] 새로고침하면 localStorage에 있는 데이터를 읽어온다. read
+// - [x] localStorage에 데이터를 저장한다. write
+// - [x] 메뉴를 수정할 때 저장
+// - [x] 메뉴를 추가할 때 저장
+// - [x] 메뉴를 삭제할 때 저장
+// - [x] 새로고침하면 localStorage에 있는 데이터를 읽어온다. read
 
 // TODO 카테고리별 메뉴판 관리
-// - [] 에스프레소 메뉴판 관리
+// - [x] 에스프레소 메뉴판 관리
 // - [] 프라푸치노 메뉴판 관리
 // - [] 블렌디드 메뉴판 관리
 // - [] 티바나 메뉴판 관리
@@ -45,7 +45,7 @@ const store = {
         localStorage.setItem('menu', JSON.stringify(menu));
     },
     getLocalStorage() {
-        localStorage.getItem('menu');
+        return JSON.parse(localStorage.getItem('menu')); // 문자열로 저장된걸 다시 json 객체 형태로 만들어줌. 문자열은 배열이 아니므로 순회하지 못한다.
     },
 };
 // setStorage로 다이렉트로 하지 않고 store 객체를 만들어준 이유? 상태 변경은 최소한의 light한 로직을 만들어서 써야함. 꼬일 수 있다.
@@ -53,45 +53,66 @@ const store = {
 function App() {
     // 상태(변할 수 있는 데이터) - 메뉴명 (개수는 굳이 저장하고 읽어오며 관리할 대상이 아님)
 
-    this.menu = [];
-    const updateMenuCount = () => {
-        const menuCount = $('#espresso-menu-list').querySelectorAll('li').length; // 그냥 querySelector이라고 하면 맨 첫번째 li 태그만 가져옴.
-        $('.menu-count').innerText = `총 ${menuCount}개`;
+    this.menu = {
+        espresso: [],
+        frappuccino: [],
+        blended: [],
+        teavana: [],
+        desert: [],
     };
 
-    const addMenuName = () => {
-        if ($('#espresso-menu-name').value === '') {
-            alert('값을 입력해주세요.');
-            return;
-        }
+    this.currentCategory = 'espresso'; // 현재 어떤 카테고리인지 상태값으로 관리.
 
-        const espressoMenuName = $('#espresso-menu-name').value;
-        this.menu.push({ name: espressoMenuName });
-        store.setLocalStorage(this.menu); // 상태가 변경되면 바로 저장하기.
-        const template = this.menu
+    this.init = () => {
+        if (store.getLocalStorage()) {
+            // 객체의 길이를 바로 구하려하면 문제가 됨. 따라서 삭제.
+            this.menu = store.getLocalStorage();
+        }
+        render();
+    };
+
+    const render = () => {
+        const template = this.menu[this.currentCategory]
             .map((menuItem, index) => {
                 return `<li data-menu-id="${index}" class="menu-list-item d-flex items-center py-2"><span class="w-100 pl-2 menu-name">
-          ${menuItem.name}</span>
-          <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-          >
-          수정
-          </button>
-          <button
-          type="button"
-          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-          >
-          삭제
-          </button>
-          </li>`;
+      ${menuItem.name}</span>
+      <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+      >
+      수정
+      </button>
+      <button
+      type="button"
+      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+      >
+      삭제
+      </button>
+      </li>`;
             })
             .join('');
         // join이 ["<li>~</li>", "<li>~</li>"]; 이걸 <li>~</li><li>~</li> 이런식으로 붙여줌.
 
-        $('#espresso-menu-list').innerHTML = template;
+        $('#menu-list').innerHTML = template;
         updateMenuCount();
-        $('#espresso-menu-name').value = ''; // 입력창을 비워줌.
+    };
+
+    const updateMenuCount = () => {
+        const menuCount = $('#menu-list').querySelectorAll('li').length; // 그냥 querySelector이라고 하면 맨 첫번째 li 태그만 가져옴.
+        $('.menu-count').innerText = `총 ${menuCount}개`;
+    };
+
+    const addMenuName = () => {
+        if ($('#menu-name').value === '') {
+            alert('값을 입력해주세요.');
+            return;
+        }
+
+        const menuName = $('#menu-name').value;
+        this.menu[this.currentCategory].push({ name: menuName }); // 에스프레소 키값에 value를 넣어주는 형태로 변경.
+        store.setLocalStorage(this.menu); // 상태가 변경되면 바로 저장하기.
+        render();
+        $('#menu-name').value = ''; // 입력창을 비워줌.
     };
 
     const updateMenuName = (e) => {
@@ -103,19 +124,23 @@ function App() {
         const $menuName = e.target.closest('li').querySelector('.menu-name');
         // 가장 가까운 li태그 가져오기.
         const updatedMenuName = prompt('메뉴명을 수정해주세요', $menuName.innerText);
-        this.menu[menuId].name = updatedMenuName;
+        this.menu[this.currentCategory][menuId].name = updatedMenuName;
         store.setLocalStorage(this.menu);
         $menuName.innerText = updatedMenuName;
         // 변수명이 길어도 의미가 명확하면 괜찮다.
     };
 
     const removeMenuName = (e) => {
+        const menuId = e.target.closest('li').dataset.menuId;
+        this.menu[this.currentCategory].splice(menuId, 1); // 배열의 특정 원소를 삭제하는 메소드. 삭제 후 결과를 반환한다.
+        // const a = this.menu.splice(menuId,1) 이렇게 하면 삭제된 원소가 반환됨.
+        store.setLocalStorage(this.menu);
         e.target.closest('li').remove();
         updateMenuCount();
     };
 
     // 수정 삭제 기능
-    $('#espresso-menu-list').addEventListener('click', (e) => {
+    $('#menu-list').addEventListener('click', (e) => {
         if (e.target.classList.contains('menu-edit-button')) {
             updateMenuName(e);
         }
@@ -128,14 +153,15 @@ function App() {
     });
 
     // form 태그가 자동으로 전송되는걸 막아준다.
-    $('#espresso-menu-form').addEventListener('submit', (e) => {
+    $('#menu-form').addEventListener('submit', (e) => {
         e.preventDefault();
     });
 
-    $('#espresso-menu-submit-button').addEventListener('click', addMenuName);
+    $('#menu-submit-button').addEventListener('click', addMenuName);
     // 이벤트 객체를 사용하지 않을 땐 이렇게 줄여서 써도 됨.
 
-    $('#espresso-menu-name').addEventListener('keypress', (e) => {
+    // 입력 기능
+    $('#menu-name').addEventListener('keypress', (e) => {
         if (e.key !== 'Enter') {
             //enter이라고 치면 안됨.
             return;
@@ -145,9 +171,21 @@ function App() {
             addMenuName();
         }
     });
+
+    $('nav').addEventListener('click', (e) => {
+        // 메뉴 사이의 빈 공간을 눌러도 e가 들어옴. 예외처리
+        const isCategoryButton = e.target.classList.contains('cafe-category-name'); // bool값으로 리턴되기 때문에.
+        if (isCategoryButton) {
+            const categoryName = e.target.dataset.categoryName;
+            this.currentCategory = categoryName;
+            $('#category-title').innerText = `${e.target.innerText} 메뉴 관리`;
+            render();
+        }
+    });
 }
 
 const app = new App();
+app.init();
 
 // refactoring
 // 재사용하는 함수는 한 곳에 모아주기
